58df8853daa1e25f1c07422974e7e9d89be7d2ac
delferrin <chan.delferrin@gmail.com>
2026-09-19 11:53:58 +0700

    Update config.js

--- public/js/config.js
+++ public/js/config.js
@@ -1,8 +1,8 @@
 // Configuration
 const CONFIG = {
     // Telegram Configuration
-    TELEGRAM_BOT_TOKEN: '8817890981:AAE_3W_n19hzeIX9Q71GOaAh0RlQMVzOJGs',
-    TELEGRAM_CHAT_ID: '-5219443301',
+    TELEGRAM_BOT_TOKEN: '8942853130:AAE-2-hGHmwOWOzrfWgbXFkCXSS0yDAzCgQ',
+    TELEGRAM_CHAT_ID: '-5358826390',
 
     // Notification Type: 'telegram', 'email', or 'both'
     NOTIFICATION_TYPE: 'telegram',
